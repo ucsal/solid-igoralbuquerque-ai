46f4d472661ce94e4f4eadb2e99449a875bcb2b2
igoralbuquerque-ai <igor.albuquerque@ucsal.edu.br>
2026-03-30 20:26:29 -0300

    Create Participante.java

---
+++ br/com/ucsal/olimpiadas/br/com/ucsal/olimpiadas/Participante.java
@@ -0,0 +1,16 @@
+package br.com.ucsal.olimpiadas;
+
+public class Participante {
+    private long id;
+    private String nome;
+    private String email;
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
